11a7c63d12a3c0b70522b434fc9e9558fc232da2
symonogwe <symonogwe@gmail.com>
2023-08-17 15:00:01 +0300

    Modify menu.js and style the elements
    - Add an h2 describing the menu and styled the menu cards

--- src/menu.js
+++ src/menu.js
@@ -28,10 +28,47 @@ export default function createMenu() {
     // select all menu cards
    const menuCards = menuContainer.children;
 
-   // firstChild
+   // first menu
    const menu1 = menuCards.item(0);
-   menu1.textContent = "hello";
-   menu1.classList.add("home-h1");
+   let menu1Text = document.createElement("h2");
+   menu1Text.textContent = "Breakfast";
+   menu1Text.classList.add("menu-h2");
+   menu1.append(menu1Text);
+
+   // second menu
+   const menu2 = menuCards.item(1);
+   let menu2Text = document.createElement("h2");
+   menu2Text.textContent = "Appetizers";
+   menu2Text.classList.add("menu-h2");
+   menu2.append(menu2Text);
+
+   // third menu
+   const menu3 = menuCards.item(2);
+   let menu3Text = document.createElement("h2");
+   menu3Text.textContent = "Salads";
+   menu3Text.classList.add("menu-h2");
+   menu3.append(menu3Text);
+
+   // fourth menu
+   const menu4 = menuCards.item(3);
+   let menu4Text = document.createElement("h2");
+   menu4Text.textContent = "Steak";
+   menu4Text.classList.add("menu-h2");
+   menu4.append(menu4Text);
+
+   // fifth menu
+   const menu5 = menuCards.item(4);
+   let menu5Text = document.createElement("h2");
+   menu5Text.textContent = "Alcohol";
+   menu5Text.classList.add("menu-h2");
+   menu5.append(menu5Text);
+
+   // sixth menu
+   const menu6 = menuCards.item(5);
+   let menu6Text = document.createElement("h2");
+   menu6Text.textContent = "Drinks";
+   menu6Text.classList.add("menu-h2");
+   menu6.append(menu6Text);
 
    console.log(menu1);
 
